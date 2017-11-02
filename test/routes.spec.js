e61046fdc59db93f08517901b8a1311fb65d2744
Travis Rollins <TROLL59910@aol.com>
2017-11-02 11:06:03 -0600

    Write tests for client side

---
+++ test/routes.spec.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+	it('should return the homepage with text', (done) => {
+		chai.request(server)
+			.get('/')
+			.end((error, response) => {
+				response.should.have.status(200);
+				response.should.be.html;
+				response.res.text.should.include('Amazon Bay');
+				done();
+      });
+    });
+
+  it('should return a 404 for a route that does not exist', (done) => {
+    chai.request(server)
+    .get('/foo')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      });
+    });
+});
