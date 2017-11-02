18a865fa901c08fd0925ab205b88178a69968aa7
Travis Rollins <TROLL59910@aol.com>
2017-11-02 14:51:47 -0600

    Add side bar functionality so that the appear to the left or right

--- public/scripts.js
+++ public/scripts.js
@@ -22,5 +22,45 @@ const fetchInventory = () => {
     .then(inventory => appendInventory(inventory))
 }
 
+const cartClick = () => {
+  $('.inventory').css('display') === 'flex' ? displayCart() : display()
+}
 
-$(document).ready(fetchInventory)
+const orderClick = () => {
+  $('.inventory').css('display') === 'flex' ? displayOrder() : display()
+}
+
+const displayCart = () => {
+  $('.inventory').css('display', 'none')
+  appendCart()
+}
+
+const appendCart = () => {
+  $('.cart').after(`
+    <section class="append-display">
+      <p>Cart Display</p>
+    </section>
+     `)
+}
+
+const displayOrder = () => {
+  $('.inventory').css('display', 'none')
+  appendOrder();
+}
+
+const appendOrder = () => {
+  $('.order-history').before(`
+    <section class="append-display">
+      <p>Order Display</p>
+    </section>
+    `)
+}
+
+const display = () => {
+  $('.inventory').css('display', 'flex')
+  $('.append-display').remove();
+}
+
+$(document).ready(fetchInventory);
+$('.cart').click(cartClick);
+$('.order-history').click(orderClick)
